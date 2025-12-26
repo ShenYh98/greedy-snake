e32ed1ea936d8af8b2aca258abeb41d6511defc5
shenyh <907184203@qq.com>
2025-12-26 16:22:56 +0800

    新增 修改 1. 修改开始页面的过场动画 2. 新增全局变量，切换不通场景时，开始页面不会重复初始化

---
+++ GlobalState.gd
@@ -0,0 +1,4 @@
+# 全局变量脚本
+extends Node
+
+@export var is_load:bool = false					# 开场动画是否加载
--- StartScreen/start_screen.gd
+++ StartScreen/start_screen.gd
@@ -1,8 +1,18 @@
 extends Control
 
+@onready var start_screen_title = $Title
+@export var title_speed: float = 80.0
+
+@onready var start_screen_btns = $MenuContainer
+@export var start_screen_btns_speed: float = 200.0
+
+@onready var snakesAnimated = $SnakesAnimated
+@export var snakesAnimated_speed:float = 200.0
+
 @export var start_button: Button
 @export var settings_button: Button
 @export var quit_button: Button
+
 
 # 当“开始游戏”按钮被按下
 func _on_start_button_pressed() -> void:
@@ -10,18 +20,32 @@ func _on_start_button_pressed() -> void:
 	print("开始游戏")
 	get_tree().change_scene_to_file("res://MainGame/MainGame.tscn")
 
+
 # 当“设置”按钮被按下
 func _on_settings_button_pressed() -> void:
 	# 切换到设置场景，请将路径替换为你的实际设置场景文件路径
 	print("打开设置")
 	get_tree().change_scene_to_file("res://Settings/Settings.tscn")
 
+
 # 当“退出游戏”按钮被按下
 func _on_quit_button_pressed() -> void:
 	# 退出游戏
 	get_tree().quit()
 
+
 func _ready() -> void:
+	if !GlobalState.is_load:
+		start_screen_title.position = Vector2(480, -70)
+		start_screen_btns.position = Vector2(-400, 425)
+		snakesAnimated.position = Vector2(200, 485)
+		snakesAnimated.play("snakes")
+	else:
+		start_screen_title.position = Vector2(480, 114)
+		start_screen_btns.position = Vector2(70, 425)
+		snakesAnimated.position = Vector2(850, 485)
+		snakesAnimated.play("snakes2")
+
 	# 检查并连接每个按钮的 pressed 信号到对应的处理函数
 	if start_button:
 		start_button.pressed.connect(_on_start_button_pressed)
@@ -29,3 +53,19 @@ func _ready() -> void:
 		settings_button.pressed.connect(_on_settings_button_pressed)
 	if quit_button:
 		quit_button.pressed.connect(_on_quit_button_pressed)
+
+
+func _process(delta: float) -> void:
+	if snakesAnimated.position.x <= 850:
+		snakesAnimated.position += Vector2.RIGHT * snakesAnimated_speed * delta
+	else:
+		snakesAnimated.play("snakes2")
+
+		if start_screen_title.position.y < 114:
+			start_screen_title.position +=  Vector2.DOWN * title_speed * delta
+			
+		if start_screen_btns.position.x < 70:
+			start_screen_btns.position += Vector2.RIGHT * start_screen_btns_speed * delta
+			
+		if start_screen_title.position.y >= 114 && start_screen_btns.position.x >= 70:
+			GlobalState.is_load = true
